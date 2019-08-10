60237125d0e7b2d3878f0cab2eceefd42a06bb29
filatov <vitaliyfilatov3110@yandex.ru>
2019-08-10 21:45:13 +0300

    Добавлен скрипт для создания варианта БД для минимального функционала (вход в систему, управления пользователями)

--- work/component_manager_min.sql
+++ work/component_manager_min.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.23, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.27, for Linux (x86_64)
 --
 -- Host: localhost    Database: component_manager
 -- ------------------------------------------------------
--- Server version	5.7.23-0ubuntu0.18.04.1
+-- Server version	5.7.27-0ubuntu0.18.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +14,10 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE SCHEMA IF NOT EXISTS component_manager;
+
+USE component_manager;
 
 --
 -- Table structure for table `addresses`
@@ -29,21 +33,14 @@ CREATE TABLE `addresses` (
   `street` varchar(255) DEFAULT NULL,
   `building` varchar(255) DEFAULT NULL,
   `object_id` int(10) unsigned DEFAULT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_addresses_1_idx` (`object_id`),
+  KEY `addresses_users_id_fk` (`user_id`),
+  CONSTRAINT `addresses_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `fk_addresses_1` FOREIGN KEY (`object_id`) REFERENCES `objects` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `addresses`
---
-
-LOCK TABLES `addresses` WRITE;
-/*!40000 ALTER TABLE `addresses` DISABLE KEYS */;
-INSERT INTO `addresses` VALUES (3,1,'Воронеж ','Улица','Дом',4);
-/*!40000 ALTER TABLE `addresses` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `complex_has_components`
@@ -63,18 +60,8 @@ CREATE TABLE `complex_has_components` (
   KEY `fk_complex_has_components_2_idx` (`component_id`),
   CONSTRAINT `fk_complex_has_components_1` FOREIGN KEY (`complex_id`) REFERENCES `complexes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_complex_has_components_2` FOREIGN KEY (`component_id`) REFERENCES `components` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1050 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `complex_has_components`
---
-
-LOCK TABLES `complex_has_components` WRITE;
-/*!40000 ALTER TABLE `complex_has_components` DISABLE KEYS */;
-INSERT INTO `complex_has_components` VALUES (1,1,1,1,'Ттв'),(2,2,1,2,'Тчт'),(3,3,1,3,'Ьчь'),(4,4,2,1,'Код1'),(5,5,2,2,'Код2'),(6,6,2,3,'Код3');
-/*!40000 ALTER TABLE `complex_has_components` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `complex_type_has_components`
@@ -93,18 +80,8 @@ CREATE TABLE `complex_type_has_components` (
   KEY `fk_complex_type_has_components_2_idx` (`component_id`),
   CONSTRAINT `fk_complex_type_has_components_1` FOREIGN KEY (`complex_type_id`) REFERENCES `complex_types` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_complex_type_has_components_2` FOREIGN KEY (`component_id`) REFERENCES `components` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `complex_type_has_components`
---
-
-LOCK TABLES `complex_type_has_components` WRITE;
-/*!40000 ALTER TABLE `complex_type_has_components` DISABLE KEYS */;
-INSERT INTO `complex_type_has_components` VALUES (1,1,1,1),(2,2,1,2),(3,3,1,3);
-/*!40000 ALTER TABLE `complex_type_has_components` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `complex_types`
@@ -115,24 +92,14 @@ DROP TABLE IF EXISTS `complex_types`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `complex_types` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned DEFAULT NULL,
   `inner_id` int(10) unsigned DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_complex_types_1_idx` (`user_id`),
-  CONSTRAINT `fk_complex_types_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  KEY `complex_types_users_id_fk` (`user_id`),
+  CONSTRAINT `complex_types_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `complex_types`
---
-
-LOCK TABLES `complex_types` WRITE;
-/*!40000 ALTER TABLE `complex_types` DISABLE KEYS */;
-INSERT INTO `complex_types` VALUES (1,1,1,'Арм');
-/*!40000 ALTER TABLE `complex_types` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `complexes`
@@ -152,25 +119,19 @@ CREATE TABLE `complexes` (
   `IPADDRESS` varchar(45) DEFAULT NULL,
   `photo_path` varchar(255) DEFAULT NULL,
   `exist_photo` tinyint(4) DEFAULT NULL,
+  `device_photo_name` varchar(45) DEFAULT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_complexes_1_idx` (`address_id`),
   KEY `fk_complexes_2_idx` (`room_id`),
   KEY `fk_complexes_3_idx` (`complex_type_id`),
+  KEY `complexes_users_id_fk` (`user_id`),
+  CONSTRAINT `complexes_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `fk_complexes_1` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_complexes_2` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_complexes_3` FOREIGN KEY (`complex_type_id`) REFERENCES `complex_types` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=92 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `complexes`
---
-
-LOCK TABLES `complexes` WRITE;
-/*!40000 ALTER TABLE `complexes` DISABLE KEYS */;
-INSERT INTO `complexes` VALUES (1,1,3,1,1,'Овов','Ьвьв','Овов','/storage/emulated/0/Android/data/com.example.vitaliy.myapplication/files/Pictures/ARM_11416504437.jpg',NULL),(2,2,3,1,1,'Идентификатор','Примечание 1','Адрес','/storage/emulated/0/Android/data/com.example.vitaliy.myapplication/files/Pictures/ARM_2-1945331368.jpg',NULL);
-/*!40000 ALTER TABLE `complexes` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `components`
@@ -181,24 +142,14 @@ DROP TABLE IF EXISTS `components`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `components` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned DEFAULT NULL,
   `inner_id` int(10) unsigned DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_components_1_idx` (`user_id`),
-  CONSTRAINT `fk_components_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  KEY `components_users_id_fk` (`user_id`),
+  CONSTRAINT `components_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `components`
---
-
-LOCK TABLES `components` WRITE;
-/*!40000 ALTER TABLE `components` DISABLE KEYS */;
-INSERT INTO `components` VALUES (1,1,1,'Системный блок '),(2,1,2,'Монитор '),(3,1,3,'Клавиатура ');
-/*!40000 ALTER TABLE `components` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `objects`
@@ -209,25 +160,47 @@ DROP TABLE IF EXISTS `objects`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `objects` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned DEFAULT NULL,
   `inner_id` int(10) unsigned DEFAULT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `device_uniq` (`user_id`,`inner_id`),
-  KEY `fk_obects_1_idx` (`user_id`),
-  CONSTRAINT `fk_obects_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `objects_users_id_fk` (`user_id`),
+  CONSTRAINT `objects_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `objects`
+-- Table structure for table `roles`
 --
 
-LOCK TABLES `objects` WRITE;
-/*!40000 ALTER TABLE `objects` DISABLE KEYS */;
-INSERT INTO `objects` VALUES (4,1,1,'Узо '),(5,1,2,'Другой объект ');
-/*!40000 ALTER TABLE `objects` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `roles_users`
+--
+
+DROP TABLE IF EXISTS `roles_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles_users` (
+  `user_id` bigint(20) unsigned NOT NULL,
+  `role_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `roles_users_ibfk_2` (`role_id`),
+  CONSTRAINT `roles_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `roles_users_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `rooms`
@@ -241,21 +214,14 @@ CREATE TABLE `rooms` (
   `inner_id` int(10) unsigned DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `address_id` int(10) unsigned DEFAULT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_rooms_1_idx` (`address_id`),
-  CONSTRAINT `fk_rooms_1` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  KEY `rooms_users_id_fk` (`user_id`),
+  CONSTRAINT `fk_rooms_1` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `rooms_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `rooms`
---
-
-LOCK TABLES `rooms` WRITE;
-/*!40000 ALTER TABLE `rooms` DISABLE KEYS */;
-INSERT INTO `rooms` VALUES (1,1,'Кабинет 1',3);
-/*!40000 ALTER TABLE `rooms` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -265,21 +231,20 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `device_id` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(254) NOT NULL,
+  `username` varchar(32) NOT NULL DEFAULT '',
+  `password` varchar(64) NOT NULL,
+  `logins` int(10) unsigned NOT NULL DEFAULT '0',
+  `last_login` int(10) unsigned DEFAULT NULL,
+  `secondname` varchar(45) DEFAULT NULL,
+  `firstname` varchar(45) DEFAULT NULL,
+  `middlename` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_username` (`username`),
+  UNIQUE KEY `uniq_email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=227 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'be0d4cdef051fc34');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -290,4 +255,22 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-03 23:22:34
+-- Dump completed on 2019-08-10 18:12:44
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'admin','Полный набор прав. Выдача прав другим пользователям.'),(2,'manager','Создание и редактирование структуры объектов. Заведение учетных записей для инженеров. доступ к задаче Пациенты'),(3,'engineer','Внесение данных об оборудовании, просмотр справочной информации по объектам.(Минимальный набор прав).'),(999,'login','Может авторизоваться на сервере, обязательна для всех пользователей');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'green@comnet-vrn.ru','green','282e2a63138df0081af5ce62987433ba92a88bdf873c6277de58d23b96bb72d4',526191,1565446991,'Скрагленко','Андрей','Владимирович');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+LOCK TABLES `roles_users` WRITE;
+/*!40000 ALTER TABLE `roles_users` DISABLE KEYS */;
+INSERT INTO `roles_users` VALUES (1,1),(1,999);
+/*!40000 ALTER TABLE `roles_users` ENABLE KEYS */;
+UNLOCK TABLES;
